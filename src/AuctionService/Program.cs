97bb381344a5139a5ca67af29fce824dde5d74fd
Efeglas <gabor.atty@gmail.com>
2023-08-11 15:26:17 +0200

    First commit

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,32 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt => {
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Gets the assembly of the application is running in and look for any class that derives from Profile.
+// Registers the mappings in memory so it's already up and good to go when need to be used.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+app.Run();
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers
+{
+    public class MappingProfiles: Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Auction, AuctionDto>().IncludeMembers(auction => auction.Item);
+            CreateMap<Item, AuctionDto>();
+            CreateMap<CreateAuctionDto, Auction>().ForMember(auction => auction.Item, options => options.MapFrom(createAuctionDto => createAuctionDto));
+            CreateMap<CreateAuctionDto, Item>();
+        }
+    }
+}
