27ab953fce7006f3866db90a564c6d70d7aa431f
Efeglas <gabor.atty@gmail.com>
2023-08-19 11:23:18 +0200

    Added consumers (delete/ update auction)

---
+++ src/SearchService/Consumers/AuctionDeletedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class AuctionDeletedConsumer : IConsumer<AuctionDeleted>
+    {
+        private readonly IMapper _mapper;
+
+        public AuctionDeletedConsumer(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        public async Task Consume(ConsumeContext<AuctionDeleted> context)
+        {
+            Console.WriteLine($"--> Consuming auction deleted: {context.Message.Id}");
+            Item item = _mapper.Map<Item>(context.Message);
+            DeleteResult result = await item.DeleteAsync();
+
+            if (!result.IsAcknowledged)
+            {
+                throw new MessageException(typeof(AuctionDeleted), "Problem deleting from MongoDB");
+            }
+        }
+    }
+}
