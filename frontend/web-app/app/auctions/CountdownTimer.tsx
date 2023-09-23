087eda0d2511255a452c79deacad864b6f2b3a72
Efeglas <gabor.atty@gmail.com>
2023-09-23 13:10:19 +0200

    Updated frontend
    Added listing for auctions
    Added pagination with Flowbite
    Added page size filters
    Added fancy way of image loading

---
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { spawn } from "child_process";
+import Countdown, { zeroPad } from "react-countdown";
+
+type Props = {
+    auctionEnd: string
+}
+
+const renderer = ({ days, hours, minutes, seconds, completed }: {days:number, hours:number, minutes:number, seconds:number, completed:boolean}) => {
+    
+    return (
+        <div className={`border-2 border-white text-white py-1 px-2 rounded-lg flex justify-center ${completed ? 'bg-red-600' : (days === 0 && hours < 10) ? 'bg-amber-600' : 'bg-green-600'}`}>
+            {completed ? (<span>Auction Finsihed</span>) : (<span suppressHydrationWarning={true}>{zeroPad(days)}:{zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}</span>)}
+        </div>
+    );
+  };
+
+const CountdownTimer = ({auctionEnd}: Props) => {
+    return (
+        <div><Countdown date={auctionEnd} renderer={renderer}/></div>
+    );
+}
+
+export default CountdownTimer;
---
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { Pagination } from "flowbite-react";
+import { useState } from "react";
+
+type Props = {
+    currentPage: number,
+    pageCount: number,
+    pageChanged: (page: number) => void
+}
+
+const AppPagination = ({currentPage, pageCount, pageChanged}: Props) => { 
+    return (
+        <Pagination currentPage={currentPage} onPageChange={e => pageChanged(e)} totalPages={pageCount} layout="pagination" showIcons className="text-blue-500 mb-5"/>
+    );
+}
+
+export default AppPagination;
